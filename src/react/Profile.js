0c6ff77b9f27480487124cc095f78324a193894e
Seimon <sieadhan@yahoo.com>
2020-03-12 13:39:15 -0400

    userProfile

--- src/react/Profile.js
+++ src/react/Profile.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Menu } from "./components";
 import { userIsAuthenticated } from "./HOCs";
+import { Card, Icon, Image } from 'semantic-ui-react'
 
 class Profile extends React.Component {
   render() {
@@ -8,9 +9,36 @@ class Profile extends React.Component {
       <>
         <Menu isAuthenticated={this.props.isAuthenticated} />
         <h2>Profile</h2>
+        const CardExampleCard = () => (
+  <Card>
+    <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' wrapped ui={false} />
+    <Card.Content>
+      <Card.Header>Matthew</Card.Header>
+      <Card.Meta>
+        <span className='date'>Joined in 2015</span>
+      </Card.Meta>
+      <Card.Description>
+        Matthew is a musician living in Nashville.
+      </Card.Description>
+    </Card.Content>
+    <Card.Content extra>
+      <a>
+        <Icon name='user' />
+        22 Friends
+      </a>
+    </Card.Content>
+  </Card>
+)
       </>
     );
   }
 }
 
 export default userIsAuthenticated(Profile);
+
+
+
+
+
+// export default CardExampleCard
+
