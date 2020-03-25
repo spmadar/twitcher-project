3ea227cbcaf52677d2bbba29218b9ee1b528b38c
Sophia Madar <spmadar@gmail.com>
2020-03-25 17:33:20 -0400

    reorganized pics and finished login screen

--- src/react/components/RegistrationForm.js
+++ src/react/components/RegistrationForm.js
@@ -2,6 +2,7 @@ import React from "react";
 import Spinner from "react-spinkit";
 import { connect } from "react-redux";
 import {user}  from "../../redux/users";
+import "./RegistrationForm.css"
 
 
 class RegistrationForm extends React.Component {
@@ -21,29 +22,32 @@ class RegistrationForm extends React.Component {
     return (
       <React.Fragment>
         <form id="registration-form" onSubmit={this.handleLogin}>
-          <label htmlFor="username">Username</label>
+          <label htmlFor="username" id= "user">Username, or email address</label>
           <input
+            id="input"
             type="text"
             name="username"
             autoFocus
             required
             onChange={this.handleChange}
           />
-          <label htmlFor="display name">Display Name</label>
+          <label htmlFor="display name" id="display">Display Name</label>
           <input
+            id="input"
             type="display"
             name="display"
             required
             onChange={this.handleChange}
           />
-          <label htmlFor="password">Password</label>
+          <label htmlFor="password" id="pass">Password</label>
           <input
+            id="input"
             type="text"
             name="password"
             onChange={this.handleChange}
         />
-          <button type="submit" disabled={loading}>
-            Sign Up!
+          <button type="submit" id="button" disabled={loading}>
+            Sign Up
           </button>
         </form>
         {loading && <Spinner name="circle" color="blue" />}
